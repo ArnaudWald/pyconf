985f4d510cec4f6b7ec1af0afbda4909d9181b52
Arnaud Wald <Arnaud Wald>
2017-07-01 18:44:51 +0900

    refactor

--- keras_tester.py
+++ keras_tester.py
@@ -9,6 +9,8 @@ Code taken from http://machinelearningmastery.com
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy
+
+
 # fix random seed for reproducibility
 numpy.random.seed(7)
 
